d355a87f5fdb1e29d8f736248cc89229b1319086
Louis Dumont <louis.dumont@m4x.org>
2019-01-22 23:01:49 +0100

    Ladder parsing function.

--- src/ladder.ml
+++ src/ladder.ml
@@ -1,3 +1,4 @@
+open Base
 open Lwt
 open Cohttp_lwt_unix
 
@@ -7,4 +8,44 @@ let get () =
 
 let print ladder =
   let ladder = Lwt_main.run ladder in
-  print_endline ladder
+  Stdio.print_endline ladder
+
+(* helper function that extracts a tuple (last name + first name, rank) from one line of the ladder file *)
+let parse_line line =
+  let max = String.length line in
+  let rec loop left_idx right_idx out =
+    if right_idx >= max then
+      let word = String.sub line ~pos:left_idx ~len:(right_idx - left_idx) in
+      word :: out
+    else
+      match line.[right_idx] with
+      | ' ' ->
+        if right_idx = left_idx then
+          loop (right_idx + 1) (right_idx + 1) out
+        else
+          let word = String.sub line ~pos:left_idx ~len:(right_idx - left_idx) in
+          loop (right_idx + 1) (right_idx + 1) (word :: out)
+      | _ -> loop left_idx (right_idx + 1) out in
+  let words =
+    List.rev (loop 0 0 [])
+    |> List.map ~f:
+      begin String.map ~f:
+          begin function
+            | '_' -> ' '
+            | c -> c
+          end
+      end
+    |> Array.of_list in
+    (words.(0) ^ " " ^ words.(1), Float.of_string words.(2))
+
+let parse ladder =
+  ladder >|= fun ladder ->
+  ladder
+  |> String.split_lines
+  |> List.tl
+  |> begin function
+    | None -> assert false
+    | Some l -> l end
+  |> List.map ~f:parse_line
+  |> Map.of_alist_reduce (module String.Caseless) ~f:(fun x _ -> x)
+    
